3370c7ff6a101f36740404090df2d15cb6adaec3
N1oH0my <h0myn1o@mail.ru>
2024-06-11 13:55:27 +0300

    SP and Room added

--- app/src/main/java/com/example/data_storage/RoomFragment.kt
+++ app/src/main/java/com/example/data_storage/RoomFragment.kt
@@ -3,13 +3,16 @@ package com.example.data_storage
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.example.data_storage.databinding.LayoutRoomBinding
 import com.example.data_storage.databinding.LayoutSpBinding
+import kotlinx.coroutines.launch
 
 class RoomFragment: Fragment(R.layout.layout_room) {
 
     private lateinit var adapter: SimpleAdapter
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -20,14 +23,32 @@ class RoomFragment: Fragment(R.layout.layout_room) {
         binding.contentRv.adapter = adapter
 
         binding.saveBtn.setOnClickListener {
-            // todo
+            saveQuery(binding.nameEt.text.toString())
         }
         binding.refreshBtn.setOnClickListener {
-            // todo
+            refresh()
         }
     }
 
     private fun renderItems(items: List<String>) {
-
+        adapter.render(items)
     }
+
+    private fun saveQuery(query: String){
+        lifecycleScope.launch{
+            getDB().quertDAO().insert(
+                QueryItemEntity(query = query)
+            )
+        }
+    }
+
+    private fun refresh(){
+        lifecycleScope.launch {
+            val queries = getDB().quertDAO().getAll()
+                .map { it.query }
+
+            renderItems(queries)
+        }
+    }
+    private fun getDB() = (requireActivity().application as App).db
 }
